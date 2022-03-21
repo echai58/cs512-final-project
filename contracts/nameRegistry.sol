18f25c337e05f8dc32d5634f36ab79e43817954a
echai58 <echai58@gmail.com>
2022-03-21 15:12:36 -0400

    authenticated functionality added to contract

--- contracts/nameRegistry.sol
+++ contracts/nameRegistry.sol
@@ -3,16 +3,24 @@ pragma solidity >=0.4.0 <0.9.0;
 contract nameRegistry {
     
     mapping (string=>string) map;
-    
+    mapping (string=>address) authMap;
 
     // Register new name, value pair
     function register(string memory name, string memory value) public returns (bool){
         bytes memory mapValue = bytes(map[name]); 
         if (mapValue.length == 0) { // key not found
             map[name] = value;
+            authMap[name] = msg.sender;
             return true;
         } 
-        return false;
+        else{
+            if (authMap[name] == msg.sender){
+                map[name] = value;
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 
     // Reads the last stored value
--- truffle-config.js
+++ truffle-config.js
@@ -39,6 +39,7 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+      // from: '0x2dC53F103F9E79678f7568E4C63266eFFDE095F2'
      }, 
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
