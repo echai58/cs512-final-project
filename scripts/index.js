736f8b03692d5a3f11d296753de5c36e1d3e07f9
Alex Wang <jianuo.wang@duke.edu>
2022-03-21 21:06:16 -0400

    iteratively send transactions to name reg contract

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,32 @@
+const TRIALS = 5
+const MAX_RAND_INT = 10;
 // scripts/index.js
 module.exports = async function main (callback) {
     try {
       // Functionality here
-      const NameRegistry = artifacts.require('nameRegistry');
-      const nameReg = await NameRegistry.deployed();
+      const NameRegistry = artifacts.require('nameRegistry')
+      const nameReg = await NameRegistry.deployed()
 
-      let val = await nameReg.read('foo');
-      console.log('Value is: ' + val);
+      // let val = await nameReg.read('foo');
+      // console.log('Value is: ' + val);
 
-      await nameReg.register('foo', 'bar2');
+      // await nameReg.register('foo', 'bar2');
       
-      val = await nameReg.read('foo');
-      console.log('Value is: ' + val);
+      // val = await nameReg.read('foo');
+      // console.log('Value is: ' + val);
+
+      for(let i = 0; i < TRIALS; i++) {
+        const key = `key-${i}`
+        const result = await nameReg.register(key, (Math.floor(Math.random() * MAX_RAND_INT).toString()))
+        console.log(result.receipt)
+
+        const val = await nameReg.read(key)
+        console.log(`Value is: ${val}`)
+      }
   
-      callback(0);
+      callback(0)
     } catch (error) {
-      console.error(error);
-      callback(1);
+      console.error(error)
+      callback(1)
     }
   };
